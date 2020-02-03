1e3f7d30201c9ec4508c54f2632d4a521f8ac42c
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-03 03:58:05 -0300

    warp server

---
+++ src/Web.hs
@@ -0,0 +1,30 @@
+module Web
+  ( main
+  )
+where
+
+import           Env                            ( Config(..)
+                                                , initialize
+                                                )
+import           Web.Router                     ( application )
+
+import           Control.Exception              ( SomeException
+                                                , catch
+                                                )
+
+import           Network.Wai.Handler.Warp
+
+main :: IO ()
+main = do
+  ctx@(_, config, _) <- initialize
+  app                <- application ctx
+  print ("Starting server on http://localhost:" ++ show (port config))
+  runSettings (warpSettings config) app
+
+onException _req e = do
+  print ("Error: " ++ show e)
+
+warpSettings :: Config -> Settings
+warpSettings config@Config { port } =
+  setPort port $ setOnException onException $ defaultSettings
+
