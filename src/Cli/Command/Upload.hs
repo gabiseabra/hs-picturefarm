554ed9714ea4013afcdb5a06cde95faced629d8e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-16 02:22:57 -0300

    cli executable

---
+++ src/Cli/Command/Upload.hs
@@ -0,0 +1,34 @@
+module Cli.Command.Upload
+  ( main
+  , prog
+  , parser
+  )
+where
+
+import           Env
+import           Service.Cloudinary
+
+import           Control.Monad                  ( void )
+
+import           Options.Applicative
+
+import           Network.HTTP.Req               ( runReq
+                                                , defaultHttpConfig
+                                                , responseBody
+                                                )
+
+main :: [FilePath] -> IO ()
+main files = do
+  config <- getEnvironment >>= loadConfig
+  void . traverse (doUpload config) $ files
+
+doUpload config file = do
+  res <- runReq defaultHttpConfig (upload config file)
+  print (responseBody res)
+
+prog :: ParserInfo (IO ())
+prog = info (main <$> parser) (progDesc "Upload files on cloudinary")
+
+parser :: Parser [FilePath]
+parser =
+  many (strArgument (metavar "FILENAME" <> help "Path of a file to upload"))
