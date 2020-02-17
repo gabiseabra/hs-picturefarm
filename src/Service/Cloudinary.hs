bdd1577d4b1c0b2144fd39c09ffc50910415441a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-16 22:10:23 -0300

    signed image upload

--- src/Service/Cloudinary.hs
+++ src/Service/Cloudinary.hs
@@ -8,12 +8,18 @@ import           Env.Config
 
 import           GHC.Generics
 
+import qualified Crypto.Hash.SHA1              as SHA1
+import qualified Data.Text                     as T
+import qualified Data.ByteString.Base16        as BS16
 import           Data.Aeson                     ( FromJSON(..) )
 import           Data.String.Conversions        ( cs )
-import qualified Data.Text                     as T
 import           Data.Tuple.Curry               ( uncurryN )
+import           Data.Time.Clock.POSIX          ( getPOSIXTime )
 
 import           Control.Applicative
+import           Control.Monad.IO.Class         ( MonadIO
+                                                , liftIO
+                                                )
 
 import           Network.HTTP.Req
 import           Network.HTTP.Client.MultipartFormData
@@ -34,11 +40,31 @@ upload config fileName = do
   body <- buildReqBody config fileName
   req POST url body jsonResponse headers
 
-buildReqBody Config { cdnUploadPreset } fileName = reqBodyMultipart
-  [ partFileSource "file" fileName
-  , partBS "upload_preset" (cs cdnUploadPreset)
-  , partBS "resource_type" "auto"
-  ]
+buildReqBody cfg@Config { cdnUploadPreset, cdnApiKey } fileName = do
+  timestamp <- now
+  let sig = signUpload cfg timestamp
+  reqBodyMultipart
+    [ partFileSource "file" fileName
+    , partBS "api_key"       (cs cdnApiKey)
+    , partBS "upload_preset" (cs cdnUploadPreset)
+    , partBS "resource_type" "auto"
+    , partBS "timestamp"     (cs $ show timestamp)
+    , partBS "signature"     (cs sig)
+    ]
+
+signUpload :: Config -> Int -> String
+signUpload Config { cdnUploadPreset, cdnApiSecret } timestamp =
+  sha1
+    $  "timestamp="
+    <> (show timestamp)
+    <> "&upload_preset="
+    <> cdnUploadPreset
+    <> cdnApiSecret
+
+now :: (MonadIO m) => m Int
+now = round . (1000 *) <$> liftIO getPOSIXTime
+
+sha1 = cs . BS16.encode . SHA1.hash . cs
 
 buildHeaders Config { cdnApiKey, cdnApiSecret } =
   basicAuth (cs cdnApiKey) (cs cdnApiSecret)
