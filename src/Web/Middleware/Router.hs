faa0b4c03151c9bdbb4fcc4e4b4fb49d1d2b8a51
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-24 05:52:48 -0300

    fix rewrites

--- src/Web/Middleware/Router.hs
+++ src/Web/Middleware/Router.hs
@@ -20,7 +20,7 @@ import           Network.Wai.Middleware.Router  ( Route
 match :: T.Text -> Application -> Route
 match path app =
   let ps = pathPieces path
-  in  (\xs -> case List.stripPrefix ps (filter ("" /=) xs) of
+  in  (\xs -> case List.stripPrefix ps (stripSlash xs) of
         Just path -> Just $ rewrite path $ app
         Nothing   -> Nothing
       )
@@ -29,11 +29,15 @@ match path app =
 dir :: T.Text -> Application -> Route
 dir path app =
   let ps = pathPieces path
-  in  (\xs -> if ps == xs then Just $ rewrite ["/"] $ app else Nothing)
+  in  (\xs -> if ps == xs then Just $ rewrite [] $ app else Nothing)
 
 rewrite :: [T.Text] -> Middleware
-rewrite path = rewritePureWithQueries $ \(_, query) -> const (path, query)
+rewrite path =
+  rewritePureWithQueries $ \(o, query) -> const (stripSlash path, query)
 
 -- | Pieces of a URL path.
 pathPieces :: T.Text -> [T.Text]
-pathPieces path = filter ("" /=) $ T.splitOn "/" path
+pathPieces path = stripSlash $ T.splitOn "/" path
+
+-- | Strip empty slashes from path. e.g: my//path/ -> my/path
+stripSlash = filter ("" /=)
